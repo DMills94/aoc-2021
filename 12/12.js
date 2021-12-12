a21d9274dcf189161c40ba80b6c32dff4253bab4
DMills94 <davidj-mills@live.co.uk>
2021-12-12 22:00:07 +0000

    day 12

--- 12/12.js
+++ 12/12.js
@@ -8,39 +8,7 @@ b-d
 A-end
 b-end`;
 
-const bigExample = `dc-end
-HN-start
-start-kj
-dc-start
-dc-HN
-LN-dc
-HN-end
-kj-sa
-kj-HN
-kj-dc`;
-
-const largeExample = `fs-end
-he-DX
-fs-he
-start-DX
-pj-DX
-end-zg
-zg-sl
-zg-pj
-pj-he
-RW-he
-fs-DX
-pj-RW
-zg-RW
-start-pj
-he-WI
-zg-he
-pj-fs
-start-RW`;
-
 // const input = exampleToArray(example);
-// const input = exampleToArray(bigExample);
-// const input = exampleToArray(largeExample);
 const input = fileToArray("./12.txt");
 
 const isSmallCave = (str) => str === str.toLowerCase();
@@ -53,19 +21,41 @@ input.forEach((line) => {
   mappedInput[attachedCave] = [...(mappedInput[attachedCave] || []), cave];
 });
 
-const calculatePossibleRoutes = (startingPoint = "start", partialRoute = [], visitedSmallCaves = new Set()) => {
+const calculatePossibleRoutesP1 = (startingPoint = "start", partialRoute = [], visitedSmallCaves = new Set()) => {
   const updatedRoute = [...partialRoute, startingPoint]; // Add the new starting point
   const updatedSmallCaves = new Set(visitedSmallCaves);
   isSmallCave(startingPoint) && updatedSmallCaves.add(startingPoint);
-  if (startingPoint === "end") return allRoutes.push(updatedRoute); // If the starting point is "end" we can finish the route
+  if (startingPoint === "end") return allP1Routes.push(updatedRoute); // If the starting point is "end" we can finish the route
 
   const possibleRoutes = mappedInput[startingPoint];
   for (let cave of possibleRoutes) {
     if (updatedSmallCaves.has(cave)) continue;
-    calculatePossibleRoutes(cave, updatedRoute, updatedSmallCaves);
+    calculatePossibleRoutesP1(cave, updatedRoute, updatedSmallCaves);
   }
 };
 
-const allRoutes = [];
-calculatePossibleRoutes();
-console.log(`P1: The total number of possible routes is ${allRoutes.length}`);
+const allP1Routes = [];
+calculatePossibleRoutesP1();
+console.log(`P1: The total number of possible routes is ${allP1Routes.length}`);
+
+// Part 2
+const calculatePossibleRoutesP2 = (startingPoint = "start", partialRoute = [], visitedSmallCaves = new Map()) => {
+  const updatedRoute = [...partialRoute, startingPoint]; // Add the new starting point
+  const updatedSmallCaves = new Map(visitedSmallCaves);
+  isSmallCave(startingPoint) &&
+    startingPoint !== "start" &&
+    updatedSmallCaves.set(startingPoint, (updatedSmallCaves.get(startingPoint) || 0) + 1);
+  if (startingPoint === "end") return allP2Routes.push(updatedRoute); // If the starting point is "end" we can finish the route
+
+  const possibleRoutes = mappedInput[startingPoint];
+  const existingDoubleVisit = Array.from(updatedSmallCaves.values()).find((visits) => visits >= 2);
+  const maxVisits = existingDoubleVisit ? 1 : 2;
+  for (let cave of possibleRoutes) {
+    if (updatedSmallCaves.get(cave) >= maxVisits || cave === "start") continue;
+    calculatePossibleRoutesP2(cave, updatedRoute, updatedSmallCaves);
+  }
+};
+
+const allP2Routes = [];
+calculatePossibleRoutesP2();
+console.log(`P2: The total number of possible routes is ${allP2Routes.length}`);
